b0b87ed9c5b6778544f358ef69aca58d91700ffa
dsledge1 <david.sledge1@gmail.com>
2025-07-07 21:54:40 -0400

    calculator added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 load_dotenv("./api.env")
 key=os.environ.get("GEMINI_API_KEY")
@@ -11,13 +11,18 @@ client = genai.Client(api_key=key)
 
 def main():
     print("Hello from agent!")
-    query=sys.argv[0]
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=query)
-    if contents == None:
+    if len(sys.argv) == 1:
         print("No user input")
         sys.exit(1)
+    user_prompt=sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            print(f"User prompt: {user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
