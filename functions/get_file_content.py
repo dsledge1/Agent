a2fc82d0a03b52f4d99793cff47fea348565090d
dsledge1 <david.sledge1@gmail.com>
2025-07-23 17:12:20 -0400

    Read implemented

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,15 +1,17 @@
 import os
 from config import read_limit
 
-def get_file_content(working_directory, file_path):
+def get_file_content(working_directory, file_path="."):
     try:
-                   
-        if not os.path.abspath(file_path).startswith(os.path.abspath(working_directory)):
+        full_path = os.path.join(working_directory, file_path)              
+        if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
             return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(full_path):
             return f'Error: "{file_path}" is not a file'
-        contents = ""
-        with open(file_path, "r") as f:
+        with open(full_path, "r") as f:
             file_content_string = f.read(read_limit)
+            if os.path.getsize(full_path) > read_limit:
+                file_content_string += f"[]...File {file_path} truncated at 10000 characters]"
+            return file_content_string
     except Exception as e:
         return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def test():
-    print(f"Result for current directory:\n{get_files_info("calculator", ".")}")
-    print(f"Result for 'pkg' directory:\n {get_files_info("calculator", "pkg")}")
-    print(f"Result for '/bin' directory:\n {get_files_info("calculator", "/bin")}")
-    print(f"Result for '../' directory:\n {get_files_info("calculator", "../")}")
+    print(f"Result for main.py\n{get_file_content("calculator", "main.py")}")
+    print(f"Result for pkg/calculator.py\n{get_file_content("calculator", "pkg/calculator.py")}")
+    print(f"Result for /bin/cat\n{get_file_content("calculator", "/bin/cat")}")
+    print(f"Result for does not exist\n{get_file_content("calculator", "pkg/does_not_exist.py")}")
 
 test()
