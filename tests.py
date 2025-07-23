4a95ed080f01cc1d0d1b17a6e55a69ca70683904
dsledge1 <david.sledge1@gmail.com>
2025-07-23 17:30:28 -0400

    Write capability implemented

--- tests.py
+++ tests.py
@@ -1,9 +1,8 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
-    print(f"Result for main.py\n{get_file_content("calculator", "main.py")}")
-    print(f"Result for pkg/calculator.py\n{get_file_content("calculator", "pkg/calculator.py")}")
-    print(f"Result for /bin/cat\n{get_file_content("calculator", "/bin/cat")}")
-    print(f"Result for does not exist\n{get_file_content("calculator", "pkg/does_not_exist.py")}")
-
+    print(f"Result for lorem\n{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}")
+    print(f"Result for pkg/morelorem.txt\n{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}")
+    print(f"Result for /tmp/temp.txt\n{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}")
+        
 test()
